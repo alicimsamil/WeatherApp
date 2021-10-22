647abe5df8e57476419038e2b0b08ac8d978882b
alicimsamil <81926983+alicimsamil@users.noreply.github.com>
2021-10-23 01:13:24 +0300

    MainFragment.kt created.

---
+++ app/src/main/java/com/alicimsamil/weatherapp/ui/screens/Main/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.alicimsamil.weatherapp.ui.screens.Main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alicimsamil.weatherapp.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+}
