ffd0cf4f33ff845791895ec21f29fc9618e18fb9
alicimsamil <81926983+alicimsamil@users.noreply.github.com>
2021-10-23 00:43:23 +0300

    Gps data fetched.

--- app/src/main/java/com/alicimsamil/weatherapp/ui/screens/Splash/SplashScreenFragment.kt
+++ app/src/main/java/com/alicimsamil/weatherapp/ui/screens/Splash/SplashScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.alicimsamil.weatherapp.ui.screens.Splash
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,10 @@ import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.alicimsamil.weatherapp.R
+import com.alicimsamil.weatherapp.viewmodel.SplashScreenViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SplashScreenFragment : Fragment() {
     private val viewModel: SplashScreenViewModel by viewModels()
@@ -21,12 +26,9 @@ class SplashScreenFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        context?.let {
-            viewModel.internetStatusCheck(it)
-        }
 
         observeInternet()
-
+        observeLocation()
 
     }
 
@@ -34,17 +36,16 @@ class SplashScreenFragment : Fragment() {
 
 
     fun observeInternet(){
+        context?.let {
+            viewModel.internetStatusCheck(it)
+        }
         viewModel.internetStatus.observe(viewLifecycleOwner, Observer {
             if (it){
                 Toast.makeText(context,"İnternet bağlantısı var.",Toast.LENGTH_LONG).show()
-
             }
             else if(!it){
-
                 Toast.makeText(context,"İnternet bağlantısı yok! Bağlantınızı kontrol ediniz.",Toast.LENGTH_LONG).show()
-
             }
-
 
         })
 
@@ -52,12 +53,26 @@ class SplashScreenFragment : Fragment() {
 
 
 
+    fun observeLocation(){
+        context?.let {
+            viewModel.getLocation(it)
+        }
+        viewModel.location.observe(viewLifecycleOwner, Observer {
+            println(it.latitude)
+        })
+    }
 
 
-
-
-
-
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == 2) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
 
 }
---
+++ app/src/main/java/com/alicimsamil/weatherapp/viewmodel/SplashScreenViewModel.kt
@@ -0,0 +1,58 @@
+package com.alicimsamil.weatherapp.viewmodel
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alicimsamil.weatherapp.util.internetCheck
+import kotlinx.coroutines.launch
+
+class SplashScreenViewModel : ViewModel(),LocationListener {
+
+    private lateinit var locationManager: LocationManager
+    val internetStatus = MutableLiveData<Boolean>()
+    val location = MutableLiveData<Location>()
+
+    fun internetStatusCheck(context:Context){
+
+        viewModelScope.launch {
+
+            internetStatus.value=internetCheck(context)
+
+        }
+
+    }
+
+
+    fun getLocation(context: Context){
+
+        locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        when {
+            ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,5f,this)
+
+            }
+            else -> {
+                ActivityCompat.requestPermissions(context as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 2)
+            }
+        }
+
+
+    }
+
+    override fun onLocationChanged(p0: Location) {
+        location.value=p0
+    }
+
+
+}
