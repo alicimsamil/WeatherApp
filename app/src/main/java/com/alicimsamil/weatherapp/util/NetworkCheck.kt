eabc6f7caf835b8e54e01960158b4cbeafb9c39c
alicimsamil <81926983+alicimsamil@users.noreply.github.com>
2021-10-22 16:34:54 +0300

    Internet connection check function created.

---
+++ app/src/main/java/com/alicimsamil/weatherapp/util/NetworkCheck.kt
@@ -0,0 +1,38 @@
+package com.alicimsamil.weatherapp.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+
+private fun internetCheck(context: Context) : Boolean{
+
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+        val network = connectivityManager.activeNetwork ?: return false
+
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+            else -> false
+        }
+
+    } else {
+
+        @Suppress("DEPRECATION") val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+        @Suppress("DEPRECATION") return networkInfo.isConnected
+
+
+    }
+
+
+
+
+
+}
